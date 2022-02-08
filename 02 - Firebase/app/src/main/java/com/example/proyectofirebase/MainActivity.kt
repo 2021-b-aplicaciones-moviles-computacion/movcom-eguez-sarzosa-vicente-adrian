8db875ad6a5fc8813f605e6d34a5b4768cd35de5
Adrian <eadepto@hotmail.com>
2022-02-08 15:53:34 -0500

    firebase inicias

---
+++ 02 - Firebase/app/src/main/java/com/example/proyectofirebase/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.proyectofirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.firebase.ui.auth.AuthUI
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Empezar el proceso de login o sign up
+        val botonLogin = findViewById<Button>(R.id.btn_login)
+        botonLogin.setOnClickListener {
+            llamarLoginUsuario()
+        }
+    }
+    fun llamarLoginUsuario(){
+        val providers = arrayListOf(
+            // lista de los proveedores
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTosAndPrivacyPolicyUrls(
+                    "https://example.com/terms.html",
+                    "https://example.com/privacy.html"
+                )
+                .build(),
+            200
+        )
+    }
+}
